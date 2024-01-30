573f83efd466b075d24b98811c7001102eede624
amr elstohy <amramr2901@gmail.com>
2024-01-30 12:38:15 +0000

    hi

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+puts ARGV[0].scan(/from:(\w+|\+?\d{11}).*to:(\w+|\+?\d{11}).*flags:(.{1,12})/).join(",")
