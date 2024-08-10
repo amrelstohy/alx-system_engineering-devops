6349cae66f6ecf263adcdd640e33c4873b4fef2f
Amr Elstohy <amramr2901@gmail.com>
2024-08-10 11:01:09 +0300

    hi

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,24 +3,25 @@
 Python script that, using this REST API.
 """
 
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
 
-count = 0
-task = ""
-id = sys.argv[1]
-name = requests.get('https://jsonplaceholder.typicode.com'
-                    f'/users?id={id}').json()
+    count = 0
+    task = ""
+    id = sys.argv[1]
+    name = requests.get('https://jsonplaceholder.typicode.com'
+                        f'/users?id={id}').json()
 
-name = name[0]["name"]
-x = requests.get('https://jsonplaceholder.typicode.com/'
-                 f'todos?userId={id}')
-tasks = x.json()
-for i in tasks:
-    if i['completed'] is True:
-        count += 1
-        task += i['title'] + '\n'
+    name = name[0]["name"]
+    x = requests.get('https://jsonplaceholder.typicode.com/'
+                    f'todos?userId={id}')
+    tasks = x.json()
+    for i in tasks:
+        if i['completed'] is True:
+            count += 1
+            task += i['title'] + '\n'
 
-print(f"Employee {name} is done with tasks({count}/{len(tasks)}):\n"
-      + task, end=(''))
+    print(f"Employee {name} is done with tasks({count}/{len(tasks)}):\n"
+        + task, end=(''))
