a83553b9b92cf6c474dccafcf0cf97be334336d4
Amr Elstohy <amramr2901@gmail.com>
2024-08-10 11:51:07 +0300

    hi

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,8 +10,6 @@ if __name__ == "__main__":
     count = 0
     task = ""
     id = sys.argv[1]
-    print(sys.argv[0])
-    print(id)
     n = requests.get('https://jsonplaceholder.typicode.com'
                      f'/users?id={id}').json()
 
