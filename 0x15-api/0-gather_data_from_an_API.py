b15e42a5b12101ee4bd189277de5c5d8b35c9679
Amr Elstohy <amramr2901@gmail.com>
2024-08-10 11:20:16 +0300

    hi

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     import requests
     import sys
 
-
     count = 0
     task = ""
     id = sys.argv[1]
@@ -16,7 +15,7 @@ if __name__ == "__main__":
 
     name = name[0]["name"]
     x = requests.get('https://jsonplaceholder.typicode.com/'
-                    f'todos?userId={id}')
+                     f'todos?userId={id}')
     tasks = x.json()
     for i in tasks:
         if i['completed'] is True:
@@ -24,4 +23,4 @@ if __name__ == "__main__":
             task += i['title'] + '\n'
 
     print(f"Employee {name} is done with tasks({count}/{len(tasks)}):\n"
-        + task, end=(''))
+          + task, end=(''))
