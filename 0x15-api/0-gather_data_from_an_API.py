ba052cc7d7eee77bb5223de009970efd6cffcb0c
Amr Elstohy <amramr2901@gmail.com>
2024-08-10 12:33:51 +0300

    hi

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
             count += 1
             task += '     ' + i['title'] + '\n'
 
-    print(f"Employee {n} is done with tasks({count}/{len(tasks)}):" + 
+    print(f"Employee {n} is done with tasks({count}/{len(tasks)}):" +
           task, end=(''))
