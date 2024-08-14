2ac453899f868fba7fbb46390701d6638fec96a7
Amr Elstohy <amramr2901@gmail.com>
2024-08-14 14:26:02 +0300

    hi

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    data = requests.get(url, allow_redirects=False)
+
+    if data.status_code == 200:
+        for i in range(1, 11):
+            print(data.json()['data']['children'][i]['data']['title'])
+    else:
+        print("None")
