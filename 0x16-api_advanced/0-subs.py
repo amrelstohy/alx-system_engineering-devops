c5d43d93173282b0d841f71c44cfd744551a1841
Amr Elstohy <amramr2901@gmail.com>
2024-08-14 12:37:16 +0300

    hi

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,8 @@ def number_of_subscribers(subreddit):
     """
     headers = {"User-Agent": "python-requests/2.22.0"}
     data = requests.get('https://www.reddit.com/r/{}/'
-                        'about.json'.format(subreddit), headers=headers, allow_redirects=False)
+                        'about.json'.format(subreddit),
+                        headers=headers, allow_redirects=False)
     if data.status_code == 200:
         data = data.json()
         return data['data']['subscribers']
