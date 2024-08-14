98467c6ffb93a8aa844dcc7e3a0235cb7bebd5f2
Amr Elstohy <amramr2901@gmail.com>
2024-08-14 12:01:38 +0300

    hi

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API
+and returns the number of subscribers.
+"""
+
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    try:
+        data = requests.get(f'https://www.reddit.com/r/{subreddit}/'
+                            'about.json').content
+        data = json.loads(data)
+        return data['data']['subscribers']
+    except Exception:
+        return 0
